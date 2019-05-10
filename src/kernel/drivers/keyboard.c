cea55e90505b6ff8e75313ca54e46c0ad645180c
dudejoe870 <dudejoe870@gmail.com>
2019-05-10 04:02:53 -0500

    Generalize the Keyboard Driver

--- src/kernel/drivers/keyboard.c
+++ src/kernel/drivers/keyboard.c
@@ -10,9 +10,12 @@
 
 #define LINE_WIDTH 79
 
-bool shift = false;
 char key_buffer[LINE_WIDTH];
 int key_buffer_index = 0;
+
+#define SCANCODES_SIZE 0xA7
+bool down_keys[SCANCODES_SIZE];
+bool caps_lock = false;
 
 #define SC_MAX 57
 const char sc_ascii_noshift[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
@@ -27,20 +30,26 @@ const char sc_ascii_shift[] = { '?', '?', '!', '@', '#', '$', '%', '^',
     'H', 'J', 'K', 'L', ':', '"', '~', '?', '|', 'Z', 'X', 'C', 'V', 
     'B', 'N', 'M', '<', '>', '?', '?', '?', '?', ' '};
 
+const uint8_t sc_non_char[] = { 0x00, 0x01, 0x0F, 0x1D, 0x2A, 0x36, 0x37, 0x38 };
+
 static void keyboard_callback(interrupt_info_t* info)
 {
     uint8_t scancode = inb(0x60);
 
-    if (scancode == 0x2A)
+    if (scancode < SCANCODES_SIZE) 
     {
-        shift = true;
-        return;
+        if (scancode == 0x3A) 
+        {
+            caps_lock = !caps_lock;
+            down_keys[0x3A] = caps_lock;
+        }
+        else down_keys[scancode] = true;
     }
-    else if (scancode == 0x2A + 0x80)
-    {
-        shift = false;
-        return;
-    }
+    else if (scancode > 0x80 && scancode != 0x3A + 0x80) down_keys[scancode - 0x80] = false;
+
+    bool shift = down_keys[0x2A] | down_keys[0x3A];
+    for (uint8_t i = 0; i < sizeof(sc_non_char) / sizeof(uint8_t); ++i)
+        if (scancode == sc_non_char[i] || scancode == sc_non_char[i] + 0x80) return;
 
     if (scancode > SC_MAX) return;
     if (scancode == ENTER)
@@ -84,4 +93,10 @@ static void keyboard_callback(interrupt_info_t* info)
 void init_keyboard()
 {
     register_interrupt_handler(IRQ1, keyboard_callback);
+}
+
+bool is_key_down(uint8_t scancode)
+{
+    if (scancode > SCANCODES_SIZE) return false;
+    return down_keys[scancode];
 }
--- src/kernel/drivers/keyboard.h
+++ src/kernel/drivers/keyboard.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
+
 bool print_input;
 bool enable_shell;
 
 void init_keyboard(void);
+bool is_key_down(uint8_t scancode);
