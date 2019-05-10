7547005ade32d8740144bc277c5f003c85eadc69
dudejoe870 <dudejoe870@gmail.com>
2019-05-10 03:03:19 -0500

    Some refactoring

--- Makefile
+++ Makefile
@@ -8,25 +8,31 @@ CFILES = $(foreach dir,$(DIRS),$(wildcard $(SRC_DIR)/$(dir)/*.c))
 ASMFILES = $(foreach dir,$(DIRS),$(wildcard $(SRC_DIR)/$(dir)/*.asm))
 CFLAGS = -ffreestanding $(foreach dir,$(DIRS),-I$(SRC_DIR)/$(dir))
 COBJFILES = $(CFILES:%.c=%.o)
-ASMOBJFILES = src/kernel/kernel_entry.o $(filter-out src/kernel/kernel_entry.o,$(ASMFILES:%.asm=%.o))
+ASMOBJFILES = src/kernel/kernel_entry.asmo $(filter-out src/kernel/kernel_entry.asmo,$(ASMFILES:%.asm=%.asmo))
 
 CC = i686-elf-gcc
 LD = i686-elf-ld
 ASM = nasm
+QEMU = qemu-system-x86_64
 
-all: $(KERN_TARGET)
+all: $(KERN_TARGET) clean
 
 $(KERN_TARGET): $(COBJFILES) $(ASMOBJFILES)
 	$(LD) -o $@ -Ttext 0x1000 $(ASMOBJFILES) $(COBJFILES) --oformat binary
 	nasm $(BOOTLOADER) -f bin -o $(TARGET_DIR)/boot_sect.bin
 	cat $(TARGET_DIR)/boot_sect.bin $@ > $(TARGET_DIR)/os_image.bin
+
+clean:
 	rm $(COBJFILES)
 	rm $(ASMOBJFILES)
 	rm $(TARGET_DIR)/boot_sect.bin
-	rm $@
+	rm $(KERN_TARGET)
+
+run-qemu:
+	$(QEMU) -drive file=build/os_image.bin,index=0,format=raw,if=floppy
 	
 $(COBJFILES): %.o : %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(ASMOBJFILES): %.o : %.asm
+$(ASMOBJFILES): %.asmo : %.asm
 	$(ASM) $< -f elf -o $@
--- src/kernel/cpu_86/interrupt.asm
+++ src/kernel/cpu_86/interrupt.asm
@@ -147,322 +147,322 @@ global syscall128
 ; 0: Divide By Zero Exception
 isr0: ; Pushes cs, eip, eflags, ss, and esp.
     cli
-    push byte 0 ; Null Error Code
-    push byte 0 ; Interrupt Number
+    push 0 ; Null Error Code
+    push 0 ; Interrupt Number
     jmp isr_common_stub
 
 ; 1: Debug Exception
 isr1:
     cli
-    push byte 0
-    push byte 1
+    push 0
+    push 1
     jmp isr_common_stub
 
 ; 2: Non Maskable Interrupt Exception
 isr2:
     cli
-    push byte 0
-    push byte 2
+    push 0
+    push 2
     jmp isr_common_stub
 
 ; 3: Int 3 Exception
 isr3:
     cli
-    push byte 0
-    push byte 3
+    push 0
+    push 3
     jmp isr_common_stub
 
 ; 4: INTO Exception
 isr4:
     cli
-    push byte 0
-    push byte 4
+    push 0
+    push 4
     jmp isr_common_stub
 
 ; 5: Out of Bounds Exception
 isr5:
     cli
-    push byte 0
-    push byte 5
+    push 0
+    push 5
     jmp isr_common_stub
 
 ; 6: Invalid Opcode Exception
 isr6:    push eax
     mov eax, ebx
     cli
-    push byte 0
-    push byte 6
+    push 0
+    push 6
     jmp isr_common_stub
 
 ; 7: Coprocessor Not Available Exception
 isr7:
     cli
-    push byte 0
-    push byte 7
+    push 0
+    push 7
     jmp isr_common_stub
 
 ; 8: Double Fault Exception (Pushes Error Code)
 isr8:
     cli
-    push byte 8
+    push 8
     jmp isr_common_stub
 
 ; 9: Coprocessor Segment Overrun Exception
 isr9:
     cli
-    push byte 0
-    push byte 9
+    push 0
+    push 9
     jmp isr_common_stub
 
 ; 10: Bad TSS Exception (Pushes Error Code)
 isr10:
     cli
-    push byte 10
+    push 10
     jmp isr_common_stub
 
 ; 11: Segment Not Present Exception (Pushes Error Code)
 isr11:
     cli
-    push byte 11
+    push 11
     jmp isr_common_stub
 
 ; 12: Stack Fault Exception (Pushes Error Code)
 isr12:
     cli
-    push byte 12
+    push 12
     jmp isr_common_stub
 
 ; 13: General Protection Fault Exception (Pushes Error Code)
 isr13:
     cli
-    push byte 13
+    push 13
     jmp isr_common_stub
 
 ; 14: Page Fault Exception (Pushes Error Code)
 isr14:
     cli
-    push byte 14
+    push 14
     jmp isr_common_stub
 
 ; 15: Reserved Exception
 isr15:
     cli
-    push byte 0
-    push byte 15
+    push 0
+    push 15
     jmp isr_common_stub
 
 ; 16: Floating Point Exception
 isr16:
     cli
-    push byte 0
-    push byte 16
+    push 0
+    push 16
     jmp isr_common_stub
 
 ; 17: Alignment Check Exception
 isr17:
     cli
-    push byte 0
-    push byte 17
+    push 0
+    push 17
     jmp isr_common_stub
 
 ; 18: Machine Check Exception
 isr18:
     cli
-    push byte 0
-    push byte 18
+    push 0
+    push 18
     jmp isr_common_stub
 
 ; 19: Reserved
 isr19:
     cli
-    push byte 0
-    push byte 19
+    push 0
+    push 19
     jmp isr_common_stub
 
 ; 20: Reserved
 isr20:
     cli
-    push byte 0
-    push byte 20
+    push 0
+    push 20
     jmp isr_common_stub
 
 ; 21: Reserved
 isr21:
     cli
-    push byte 0
-    push byte 21
+    push 0
+    push 21
     jmp isr_common_stub
 
 ; 22: Reserved
 isr22:
     cli
-    push byte 0
-    push byte 22
+    push 0
+    push 22
     jmp isr_common_stub
 
 ; 23: Reserved
 isr23:
     cli
-    push byte 0
-    push byte 23
+    push 0
+    push 23
     jmp isr_common_stub
 
 ; 24: Reserved
 isr24:
     cli
-    push byte 0
-    push byte 24
+    push 0
+    push 24
     jmp isr_common_stub
 
 ; 25: Reserved
 isr25:
     cli
-    push byte 0
-    push byte 25
+    push 0
+    push 25
     jmp isr_common_stub
 
 ; 26: Reserved
 isr26:
     cli
-    push byte 0
-    push byte 26
+    push 0
+    push 26
     jmp isr_common_stub
 
 ; 27: Reserved
 isr27:
     cli
-    push byte 0
-    push byte 27
+    push 0
+    push 27
     jmp isr_common_stub
 
 ; 28: Reserved
 isr28:
     cli
-    push byte 0
-    push byte 28
+    push 0
+    push 28
     jmp isr_common_stub
 
 ; 29: Reserved
 isr29:
     cli
-    push byte 0
-    push byte 29
+    push 0
+    push 29
     jmp isr_common_stub
 
 ; 30: Reserved
 isr30:
     cli
-    push byte 0
-    push byte 30
+    push 0
+    push 30
     jmp isr_common_stub
 
 ; 31: Reserved
 isr31:
     cli
-    push byte 0
-    push byte 31
+    push 0
+    push 31
     jmp isr_common_stub
 
 ; IRQs
 irq0:
 	cli
-	push byte 0
-	push byte 32
+	push 0
+	push 32
 	jmp irq_common_stub
 
 irq1:
 	cli
-	push byte 1
-	push byte 33
+	push 1
+	push 33
 	jmp irq_common_stub
 
 irq2:
 	cli
-	push byte 2
-	push byte 34
+	push 2
+	push 34
 	jmp irq_common_stub
 
 irq3:
 	cli
-	push byte 3
-	push byte 35
+	push 3
+	push 35
 	jmp irq_common_stub
 
 irq4:
 	cli
-	push byte 4
-	push byte 36
+	push 4
+	push 36
 	jmp irq_common_stub
 
 irq5:
 	cli
-	push byte 5
-	push byte 37
+	push 5
+	push 37
 	jmp irq_common_stub
 
 irq6:
 	cli
-	push byte 6
-	push byte 38
+	push 6
+	push 38
 	jmp irq_common_stub
 
 irq7:
 	cli
-	push byte 7
-	push byte 39
+	push 7
+	push 39
 	jmp irq_common_stub
 
 irq8:
 	cli
-	push byte 8
-	push byte 40
+	push 8
+	push 40
 	jmp irq_common_stub
 
 irq9:
 	cli
-	push byte 9
-	push byte 41
+	push 9
+	push 41
 	jmp irq_common_stub
 
 irq10:
 	cli
-	push byte 10
-	push byte 42
+	push 10
+	push 42
 	jmp irq_common_stub
 
 irq11:
 	cli
-	push byte 11
-	push byte 43
+	push 11
+	push 43
 	jmp irq_common_stub
 
 irq12:
 	cli
-	push byte 12
-	push byte 44
+	push 12
+	push 44
 	jmp irq_common_stub
 
 irq13:
 	cli
-	push byte 13
-	push byte 45
+	push 13
+	push 45
 	jmp irq_common_stub
 
 irq14:
 	cli
-	push byte 14
-	push byte 46
+	push 14
+	push 46
 	jmp irq_common_stub
 
 irq15:
 	cli
-	push byte 15
-	push byte 47
+	push 15
+	push 47
 	jmp irq_common_stub
 
-; Interrupt 128 is for syscalls (like Unix)
+; Interrupt 128 is for syscalls (like Linux)
 syscall128:
     cli
-    push byte 0
-    push byte 128
+    push 0
+    push 128
     jmp syscall_common_stub
